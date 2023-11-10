c37a31b3cc784b810b526d1ed06f106427664c2b
kawahata-r <kawahata12191219@gmail.com>
2023-11-11 04:21:40 +0900

    sound volume manager

--- Assets/Scripts/PlaySound.cs
+++ Assets/Scripts/PlaySound.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
  
+
 public class PlaySound : MonoBehaviour
 {
-    AudioSource audioSource;
+    public static AudioSource audioSource;
  
     void Start()
     {
+        //DontDestroyOnLoad (this);
         audioSource = GetComponent<AudioSource>();
     }
  
@@ -15,4 +17,8 @@ public class PlaySound : MonoBehaviour
     {
         audioSource.PlayOneShot(audioSource.clip);
     }
+    public void SoundSliderOnValueChange(float newSliderValue)
+	{
+		audioSource.volume = newSliderValue;
+	}
 }
