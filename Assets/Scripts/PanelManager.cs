46a9997063a643eab56741635901d16867f0cafe
kokita <valley0037@gmail.com>
2023-11-29 23:57:01 +0900

    panelmanager.csを変更

--- Assets/Scripts/PanelManager.cs
+++ Assets/Scripts/PanelManager.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class PanelManager : MonoBehaviour
 {
     [SerializeField] GameObject panel;
-    [SerializeField] GameObject button1;
-    [SerializeField] GameObject button2;
 
     // Start is called before the first frame update
     void Start()
@@ -16,14 +14,10 @@ public class PanelManager : MonoBehaviour
     public void ActivatePanel()
     {
         panel.SetActive(true);
-        button1.SetActive(false);
-        button2.SetActive(true);
     }
 
     public void InactivatePanel()
     {
         panel.SetActive(false);
-        button1.SetActive(true);
-        button2.SetActive(false);
     }
 }
