089469bec722dc378135afb76a65a7ca5548eb4d
kokita <valley0037@gmail.com>
2023-11-30 07:02:08 +0900

    ボタン上にカーソルがあるときにスクロール出来ないことの修正

--- Assets/Scripts/OnMouseScaleChange.cs
+++ Assets/Scripts/OnMouseScaleChange.cs
@@ -1,34 +1,24 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using System.Collections;
-using System.Collections.Generic;
-public class OnMouseScaleChange : MonoBehaviour
+
+public class OnMouseScaleChange : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] float onScale = 1.1f;
-    private EventTrigger eventTrigger;
+    private Vector3 originalScale;
+
     void Start()
     {
-        EventTrigger.Entry entry_Enter = new EventTrigger.Entry();
-        eventTrigger = gameObject.AddComponent<EventTrigger>();
-
-        entry_Enter.eventID = EventTriggerType.PointerEnter;
-        entry_Enter.callback.AddListener((eventDate) => OnPointEnter());
-        eventTrigger.triggers.Add(entry_Enter);
-
-        EventTrigger.Entry entry_Exit = new EventTrigger.Entry();
-        entry_Exit.eventID = EventTriggerType.PointerExit;
-        entry_Exit.callback.AddListener((eventDate) => OnPointExit());
-        eventTrigger.triggers.Add(entry_Exit);
-
+        originalScale = transform.localScale;
     }
-    
-    public void OnPointEnter()
+
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        this.transform.localScale = new Vector3(onScale, onScale, onScale);
+        transform.localScale = originalScale * onScale;
     }
-    public void OnPointExit()
+
+    public void OnPointerExit(PointerEventData eventData)
     {
-        this.transform.localScale = new Vector3(1f, 1f, 1f);
+        transform.localScale = originalScale;
     }
 }
