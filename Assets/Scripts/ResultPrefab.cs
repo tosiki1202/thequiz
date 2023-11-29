35b666ac78dec925185febab102d297f42af20f5
kokita <valley0037@gmail.com>
2023-11-29 23:55:15 +0900

    リザルトの正誤部分を画像にした

--- Assets/Scripts/ResultPrefab.cs
+++ Assets/Scripts/ResultPrefab.cs
@@ -5,15 +5,21 @@ using UnityEngine.UI;
 using TMPro;
 public class ResultPrefab : MonoBehaviour
 {
+    public Sprite maru;
+    public Sprite batu;
     public TextMeshProUGUI id;
-    public TextMeshProUGUI ans;
+    public Image ans;
     public TextMeshProUGUI time;
     public Button detailButton;
 
     public void RegisterResultPrefab(int id, bool correct, float time)
     {
+        if(correct == true){
+            this.ans.sprite = maru;
+        }else{
+            this.ans.sprite = batu;
+        }
         this.id.text = id.ToString();
-        this.ans.text = correct.ToString();
         this.time.text = time.ToString("f2");
     }
 }
