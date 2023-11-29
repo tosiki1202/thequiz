6a6b86de5799ac532cbcb90042719ec6bc1eb4e3
kokita <valley0037@gmail.com>
2023-11-29 23:57:56 +0900

    panelmanager.csからstartとボタン操作を削除

--- Assets/Scripts/PanelManager.cs
+++ Assets/Scripts/PanelManager.cs
@@ -5,12 +5,6 @@ using UnityEngine;
 public class PanelManager : MonoBehaviour
 {
     [SerializeField] GameObject panel;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        panel.SetActive(false);
-    }
     public void ActivatePanel()
     {
         panel.SetActive(true);
